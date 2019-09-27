662093836ab0792afd78df472d2b0bc4e4f36371
jmjablons <juda.jablonska@gmail.com>
2019-09-27 19:35:21 +0200

    init#2

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -1,15 +1,19 @@
 package com.company.enroller.controllers;
 
 import com.company.enroller.model.Meeting;
+import com.company.enroller.model.Participant;
 import com.company.enroller.persistence.MeetingService;
 import com.company.enroller.persistence.ParticipantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URISyntaxException;
 import java.util.Collection;
 
 @RestController
@@ -24,8 +28,59 @@ public class MeetingRestController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public ResponseEntity<?> getMeetings() {
-
         Collection<Meeting> meetings = meetingService.getAll();
         return new ResponseEntity<Collection<Meeting>>(meetings, HttpStatus.OK);
     }
+    
+    @SuppressWarnings("rawtypes")
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getMeeting(@PathVariable("id") long id) {
+	     Meeting meeting = meetingService.findById(id);
+	     if (meeting == null) {
+	         return new ResponseEntity(HttpStatus.NOT_FOUND);
+	     }
+	     return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
+	 }
+		
+	@SuppressWarnings("rawtypes")
+	@RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<?> addMeeting(@RequestBody Meeting meeting) {
+        Meeting addedMeeting = meetingService.findByTitle(meeting.getTitle());
+        if (addedMeeting != null) {
+            return new ResponseEntity<>(
+            		"Unable to create. A meeting named " + 
+            meeting.getTitle() + 
+            " already exist.", 
+            HttpStatus.CONFLICT);
+        }
+        meetingService.add(meeting);
+        return new ResponseEntity<Meeting>(meeting, HttpStatus.CREATED);
+    }
+	 
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+		public ResponseEntity<?> deleteMeeting(@PathVariable("id") long id) throws URISyntaxException {
+		     Meeting meeting = meetingService.findById(id);
+		     if (meeting == null) {
+		         return new ResponseEntity(HttpStatus.NOT_FOUND);
+		     }
+		     meetingService.delete(meeting);
+		     return new ResponseEntity(HttpStatus.GONE);
+		 }
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.POST)
+	public ResponseEntity<?> addMeetingParticipant(@PathVariable("id") long id,
+			@RequestBody Participant participant) throws URISyntaxException {
+	     Meeting meeting = meetingService.findById(id);
+	     if (meeting == null) {
+	         return new ResponseEntity(HttpStatus.NOT_FOUND);
+	     }
+	     meeting.addParticipant(participant);
+	     meetingService.update(meeting);
+	     return new ResponseEntity(HttpStatus.OK);
+	 }	
+	
+    public Collection<Participant> getMeetingParticipants(long id) {
+	    return meetingService.findById(id).getParticipants();
+    }
+
 }
--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -20,6 +20,30 @@ public class MeetingService {
         Query query = connector.getSession().createQuery(hql);
         return query.list();
     }
+	public Meeting findById(long id){
+		return (Meeting) connector.getSession().get(Meeting.class, id);
+	}
+	
+	public void add(Meeting meeting) {
+		org.hibernate.Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(meeting);
+		transaction.commit();
+	}
 
+	public void delete(Meeting meeting) {
+		org.hibernate.Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(meeting);
+		transaction.commit();
+	}
+
+	public void update(Meeting meeting) {
+		org.hibernate.Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().update(meeting);
+		transaction.commit();
+	}
+
+	public Meeting findByTitle(String title) {
+		return (Meeting) connector.getSession().get(Meeting.class, (String) title);
+	}
 
 }
